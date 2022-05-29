d2a0ea9f9b8b2ec8690c538a72b8c6898e02cbbe
shamnas-sahalat <105098573+shamnas-sahalat@users.noreply.github.com>
2022-05-29 09:42:42 +0300

    Initial
    Initial commit

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$phone=$_POST['phone'];
+$address= $_POST['address'];
+$message= $_POST['message'];
+$to = "m.shamnas@sahalat.qa";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n phone =" . $phone . "address = ". $address . "\r\n message =" . $message;
+$headers = "From: noreply@sahalat.qa" . "\r\n" .
+"CC: shamnasvanimel229@gmail.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
